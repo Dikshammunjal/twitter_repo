70b435c15d39c35b9daa6f333bdf7cbcbf896462
Diksha Munjal <57708209+Dikshammunjal@users.noreply.github.com>
2022-08-25 17:23:39 +0530

    Create create_ocir_secret.sh

---
+++ create_ocir_secret.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+DOCKERURL=$1
+USERNAME=$2
+PASSWORD=$3
+EMAIL=$4
+
+
+
+
+kubectl create secret docker-registry ocirloginsecret --docker-server=$DOCKERURL  --docker-username=$USERNAME --docker-password=$PASSWORD  --docker-email=$EMAIL
+
