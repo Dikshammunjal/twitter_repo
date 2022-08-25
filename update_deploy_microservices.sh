0d085e52b80da10e752e0912cafb83a778e4e3a2
Diksha Munjal <57708209+Dikshammunjal@users.noreply.github.com>
2022-08-26 00:45:20 +0530

    Update update_deploy_microservices.sh

--- update_deploy_microservices.sh
+++ update_deploy_microservices.sh
@@ -1,14 +1,25 @@
 #!/bin/sh -x
 
-changens=$1
-changedeploymentname=$2
-changeimage=$3
-changeport=$4
-changeservice=$5
-template=$6
+DOCKERURL=$1
+USERNAME=$2
+PASSWORD=$3
+EMAIL=$4
+changens=$5
+changedeploymentname=$6
+changeimage=$7
+changeport=$8
+changeservice=$9
 
- cp deployment_template.yaml "$changedeploymentname"_deployment_template.yaml
 
- /usr/bin/sed -i -e 's/changens/'$changens'/g' -e 's/changedeploymentname/'$changedeploymentname'/g' -e 's/changeimage/'$changeimage'/g' -e 's/changeport/'$changeport'/g' -e 's/changeservice/'$changeservice'/g' "$changedeploymentname"_deployment_template.yaml
 
-kubectl apply -f "$changedeploymentname"_deployment_template.yaml
+
+kubectl create ns $changens
+
+kubectl create secret docker-registry ocirloginsecret --docker-server=$DOCKERURL  --docker-username=''$USERNAME'' --docker-password=''$PASSWORD''  --docker-email=''$EMAIL'' -n $changens
+
+
+cp deployment_template.yaml "$changedeploymentname"_deployment_template.yaml
+
+ /usr/bin/sed -i -e 's%changens%'$changens'%g' -e 's%changedeploymentname%'$changedeploymentname'%g' -e 's%changeimage%'$changeimage'%g' -e 's%changeport%'$changeport'%g' -e 's%changeservice%'$changeservice'%g' "$changedeploymentname"_deployment_template.yaml
+
+kubectl apply -f "$changedeploymentname"_deployment_template.yaml                                                                 
